e2ba98ba780164131184ca63cfd9cdcc3fb5ada2
Dheshoju Kalyan Kumar <kalyanspade.n@gmail.com>
2020-06-26 06:24:58 +0530

    initial 3

--- index.js
+++ index.js
@@ -4,4 +4,4 @@ const port = 3000 || process.env.PORT
 
 app.get('/', (req, res) => res.send('Hello World!'))
 
-app.listen(port, () => console.log(`Example app listening at http://localhost:${port}`))
+app.listen(process.env.PORT || 5000, () => console.log(`Example app listening at http://localhost:${port}`))
